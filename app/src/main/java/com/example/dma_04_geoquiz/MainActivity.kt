d0bb2fe6c24a6d1f8ae4527349fc3298223587c2
hikki.HIN <hikki.hin@gmail.com>
2023-11-08 19:08:15 +0700

    4.1 lab is worked +count right answers +Russian language

--- app/src/main/java/com/example/dma_04_geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/dma_04_geoquiz/MainActivity.kt
@@ -15,8 +15,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
-    var CurrentQuestNum = 1
-    var RightAnsNum = 0
+    var currentQuestNum = 1
+    var rightAnsNum = 0
+    var questsCount = 6
     private val quizViewModel: QuizViewModel by
     lazy {
         ViewModelProviders.of(this).get(QuizViewModel::class.java)
@@ -27,27 +28,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
-         falseButton = findViewById(R.id.false_button)
+        falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
         trueButton.setOnClickListener {
+            if(currentQuestNum == questsCount + 1){
+                currentQuestNum = 1
+                rightAnsNum = 0
+                nextButton.text = "Дальше"
+            }
             checkAnswer(true)
             uncklickButtons()
-            if(CurrentQuestNum == 6) { nextButton.setVisibility(View.GONE) }
+            if(currentQuestNum == questsCount) {
+                /*nextButton.setVisibility(View.GONE)*/
+                nextButton.text = "Попробовать снова"
+                questionTextView.text = "Вы дали ${rightAnsNum} из ${questsCount} верных ответов!"
+
+            }
         }
         falseButton.setOnClickListener {
+            if(currentQuestNum == questsCount + 1){
+                currentQuestNum = 1
+                rightAnsNum = 0
+                nextButton.text = "Дальше"
+            }
             checkAnswer(false)
             uncklickButtons()
-            if(CurrentQuestNum == 6) { nextButton.setVisibility(View.GONE) }
+            if(currentQuestNum == questsCount) {
+                nextButton.text = "Попробовать снова"
+                questionTextView.text = "Вы дали ${rightAnsNum} верных ответов!"
+
+            }
         }
         nextButton.setOnClickListener {
-            CurrentQuestNum =+1
+            currentQuestNum += 1
 
             quizViewModel.moveToNext()
             updateQuestion()
             trueButton.setVisibility(View.VISIBLE)
             falseButton.setVisibility(View.VISIBLE)
             nextButton.setVisibility(View.GONE)
+
         }
         updateQuestion()
         /* 84 страница книги*/
@@ -64,7 +85,7 @@ class MainActivity : AppCompatActivity() {
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = quizViewModel.currentQuestionAnswer
         val messageResId = if (userAnswer == correctAnswer) {
-            RightAnsNum =+1
+            rightAnsNum +=1
             R.string.correct_toast } else { R.string.incorrect_toast }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
